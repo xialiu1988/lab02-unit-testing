441c29f521eab968d7c000f88fbf39b631ba1c0e
xia LIU <xia.liu2@outlook.com>
2019-01-09 11:21:39 -0800

    setup files for lab02 start writing main method

---
+++ lab02-unit testing/lab02-unit testing/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace lab02_unit_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************************************");
+            Console.WriteLine("************************************************");
+            Console.WriteLine("************************************************");
+            Console.WriteLine("*********Hello!**** Welcome to Piggy Bank!******");
+            Console.WriteLine("************************************************");
+            Console.WriteLine("************************************************");
+            Console.WriteLine("************************************************");
+
+            Console.WriteLine("Press a number:");
+            Console.WriteLine("1:Deposit");
+            Console.WriteLine("2:Withdrawl");
+            Console.WriteLine("3:View balance");
+            Console.WriteLine("4:Exit");
+
+
+            bool run = true;
+            Decimal myBalance = 5000;
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
